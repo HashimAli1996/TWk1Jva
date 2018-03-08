ab8589317352fb7370d369d8048261f8f1fd3556
HashimAli1996 <hashim20070@hotmail.co.uk>
2018-03-08 10:49:50 +0000

    multiarray fille added

---
+++ multiArrays.java
@@ -0,0 +1,43 @@
+
+public class multiArrays {
+	
+	
+	public static void main(String[] args) {
+		int [][] twoDArray = new int[3][3];
+		
+		//out loop to change row
+		for (int i = 0; i < twoDArray.length; i++) {
+			//goes through all ints in a row
+			for(int j = 0; j < twoDArray[i].length; j++) {
+				System.out.print(twoDArray[i][j]);
+			}
+			System.out.println("");
+		}
+		
+		
+		int [][] grid = new int [5][20]; 
+	
+		
+		
+		for (int i = 0; i < grid.length; i++ ) {
+			System.out.print("[]");
+			for (int j = 0; j < grid[i].length; j++ ) {	
+				System.out.print("[]");
+					}
+			System.out.println();
+		
+			/*git order
+			 * git status
+			 * git clone WEBSITE URL
+			 * git add .
+			 * git commit -m"fujfhufhfu"
+			 * git push
+			 */
+			
+
+		}
+		
+	}
+	
+	
+}
